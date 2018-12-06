6d68e4df3c09dcd1f23e9c2c604484495244314c
Aleksander <rit>
2018-12-06 16:21:02 +0300

    Coverage

--- src/test/java/ru/itpark/CashBackServiceTest.java
+++ src/test/java/ru/itpark/CashBackServiceTest.java
@@ -11,5 +11,10 @@ class CashbackServiceTest {
         CashBackService cashback = new CashBackService();
         int bonus = cashback.calculateCashback(1000, 2000, 3000);
         assertEquals(1010, bonus);
+    }@Test
+    void calculateCashbackMaxLimit() {
+        CashBackService cashback = new CashBackService();
+        int bonus = cashback.calculateCashback(90000000, 900000000, 900000000);
+        assertEquals(9000, bonus);
     }
 }
